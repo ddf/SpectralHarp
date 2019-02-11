6d13fd405a02896b75c03d612d334ce94271e76c
Damien Quartz <dcomposer@gmail.com>
2019-02-11 15:42:05 -0600

    update config.h to match old resource.h values

--- config.h
+++ config.h
@@ -1,29 +1,29 @@
 #define PLUG_NAME "SpectralHarp"
-#define PLUG_MFR "DamienQuartz"
-#define PLUG_VERSION_HEX 0x00010000
-#define PLUG_VERSION_STR "1.0.0"
-#define PLUG_UNIQUE_ID 'IWxO'
-#define PLUG_MFR_ID 'Acme'
-#define PLUG_URL_STR "https://iplug2.github.io"
-#define PLUG_EMAIL_STR "spam@me.com"
-#define PLUG_COPYRIGHT_STR "Copyright 2019 Acme Inc"
+#define PLUG_MFR "Damien Quartz"
+#define PLUG_VERSION_HEX 0x00020000
+#define PLUG_VERSION_STR "2.0.0"
+#define PLUG_UNIQUE_ID 'Shrp'
+#define PLUG_MFR_ID 'Cpmt'
+#define PLUG_URL_STR "https://damikyu.itch.io/spectralharp"
+#define PLUG_EMAIL_STR "info@compartmental.net"
+#define PLUG_COPYRIGHT_STR "Copyright 2019 Damien Quartz"
 #define PLUG_CLASS_NAME SpectralHarp
 
 #define BUNDLE_NAME "SpectralHarp"
-#define BUNDLE_MFR "DamienQuartz"
-#define BUNDLE_DOMAIN "com"
+#define BUNDLE_MFR "compartmental"
+#define BUNDLE_DOMAIN "net"
 
-#define PLUG_CHANNEL_IO "2-2"
+#define PLUG_CHANNEL_IO "1-1 2-2"
 
 #define PLUG_LATENCY 0
-#define PLUG_TYPE 0
-#define PLUG_DOES_MIDI_IN 0
+#define PLUG_TYPE 1
+#define PLUG_DOES_MIDI_IN 1
 #define PLUG_DOES_MIDI_OUT 0
 #define PLUG_DOES_MPE 0
 #define PLUG_DOES_STATE_CHUNKS 0
 #define PLUG_HAS_UI 1
-#define PLUG_WIDTH 600
-#define PLUG_HEIGHT 600
+#define PLUG_WIDTH 1024
+#define PLUG_HEIGHT 400
 #define PLUG_FPS 60
 #define PLUG_SHARED_RESOURCES 0
 
@@ -40,7 +40,7 @@
 #define AAX_PLUG_CATEGORY_STR "Effect"
 #define AAX_DOES_AUDIOSUITE 1
 
-#define VST3_SUBCATEGORY "Fx"
+#define VST3_SUBCATEGORY "Instrument|Synth"
 
 #define APP_NUM_CHANNELS 2
 #define APP_N_VECTOR_WAIT 0
