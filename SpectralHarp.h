b6d80d99b33c753fb32db9cd07806175adb41e6e
Damien Quartz <dcomposer@gmail.com>
2019-02-21 14:50:02 -0600

    add IPLUG_DSP and IPLUG_EDITOR defines
    + not sure if exactly correct, could not get WAM building on Windows

--- SpectralHarp.h
+++ SpectralHarp.h
@@ -17,14 +17,19 @@ public:
 	SpectralHarp(IPlugInstanceInfo instanceInfo);
 	~SpectralHarp();
 
+#if IPLUG_DSP
 	void OnReset() override;
 	void OnParamChange(int paramIdx) override;
 	void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
-  void OnIdle() override;
-	
-	int UnserializeState(const IByteChunk& chunk, int startPos) override;
+  void ProcessMidiMsg(const IMidiMsg& msg) override;
 
-	void ProcessMidiMsg(const IMidiMsg& msg) override;
+  // handle messages from the UI
+  bool OnMessage(int messageTag, int controlTag, int dataSize, const void* pData) override;
+  // transmit data to the UI
+  void OnIdle() override;
+#endif
+	
+	int UnserializeState(const IByteChunk& chunk, int startPos) override;	
 
 	// catch the About menu item to display what we wants in a box
 	bool OnHostRequestingAboutBox() override;
@@ -33,9 +38,6 @@ public:
 
   // update the UI with current midi mappings
   void OnUIOpen() override;
-
-  // handle messages from the UI
-  bool OnMessage(int messageTag, int controlTag, int dataSize, const void* pData) override;
 
 #if APP_API
 	void BroadcastParamChange(const int paramIdx);
@@ -58,6 +60,7 @@ private:
   WDL_String      mIniPath;
 	bool 					  mIsLoading;
 
+#if IPLUG_DSP
 	float					  mGain;
 	float					  mPluckX;
 	float					  mPluckY;
@@ -66,13 +69,14 @@ private:
 	SpectralGen               specGen;
 	Minim::BitCrush           bitCrush;
 	Minim::TickRate           tickRate;
+
+  IMidiQueue				  mMidiQueue;
+  std::vector<IMidiMsg>	  mNotes;
   StringControl::Capture    spectrumCapture;
+#endif
 
 #if APP_API  
 	// for each param, which midi control change should set its value
 	IMidiMsg::EControlChangeMsg controlChangeForParam[kNumParams];
-#endif
-
-	IMidiQueue				  mMidiQueue;
-	std::vector<IMidiMsg>	  mNotes;	
+#endif		
 };
